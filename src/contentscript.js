d2b6ee65aace443468e8b8c7a1a93f071c6c6ae9
Jacob Meacham <jacob.e.meacham@gmail.com>
2016-01-08 13:54:07 -0800

    Redact everything.

--- src/contentscript.js
+++ src/contentscript.js
@@ -2,29 +2,34 @@ import $ from 'jquery';
 import moment from 'moment';
 import _each from 'lodash/collection/each';
 
-const birthdayText = $('.bday').text();
-const date = moment(birthdayText);
-
-function permuteMoment(moment) {
-  return [moment.format('MMMM DD, YYYY'), moment.format('DD MMMM YYYY')];
+function textElements(selector) {
+  return $(selector).contents().filter((_, element) => {
+    return element.nodeType === 3;
+  });
 }
 
-const moments = permuteMoment(date);
+// Find a birthday:
+let birthdayText = '';
+if (document.URL.indexOf('wikipedia') !== -1) {
+  birthdayText = $('.bday').text();
+} else {
+  birthdayText = $('._Xbe').text();
+}
 
-$('body *').contents().filter(function() { // TODO: Polyfill so that `this` works correctly
-  return this.nodeType === 3;
-}).each(function() {
-  const node = this;
-  _each(moments, (val) => {
-    node.textContent = node.textContent.replace(val, 'REDACTED');
-  })
-});
+if (!birthdayText) {
+  // Find something that looks like it might be a birthday:
+  const regex = /born (.*)\)/;
+  textElements($('body *')).each((_, element) => {
+    const match = regex.exec(element.textContent);
+    if (match) {
+      birthdayText = match[1];
+      return false; // Found one, so exit early.
+    }
+  });
+}
 
-// $('._Xbe').text(() => {
-//     return 'Redacted';
-// });​​​​​
-
-// Hash the date so that it's reproducible
+// Still no birthday text, so move on.
+// Otherwise:
 // 1. Get the birthday, and death day (if applicable)
 // 2. Calculate age, if death day, and make sure that birthday and death day are separated by at least that much. If no death day, make
 // up a current day
@@ -32,4 +37,18 @@ $('body *').contents().filter(function() { // TODO: Polyfill so that `this` work
 // should go up
 // 4. Permute the original age and find all of the places where that exists in the page, then change them (maybe just in overview).
 // 5. Also change the age to a new number
-// 6. Also need to grab _Xbe on google
+// Hash the date so that it's reproducible
+if (birthdayText) {
+  const date = moment(birthdayText);
+  console.log(date);
+  const permutedDates = [date.format('MMMM D, YYYY'), date.format('D MMMM YYYY')];
+  console.log(permutedDates[0] + ' ' + permutedDates[1]);
+
+  textElements($('body *')).each((_, element) => {
+    // Use each permuted date to redact.
+    _each(permutedDates, (val) => {
+      //console.log(val + ', ' + element.textContent);
+      element.textContent = element.textContent.replace(val, 'REDACTED');
+    });
+  });
+}
